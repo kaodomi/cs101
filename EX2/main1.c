e1f5d345a8974d57ecdbff2ad9a896a08d416471
kaodomi <151603952+kaodomi@users.noreply.github.com>
2023-12-14 15:08:24 +0800

    Update main1.c

--- EX2/main1.c
+++ EX2/main1.c
@@ -2,10 +2,10 @@ int main()
 {
     int i = 8;
     if (i & (i-1)) {
-        printf("false");
+        printf("true");
     }
     else {
-        printf("true");
+        printf("false");
     }
     return 0;
 }
