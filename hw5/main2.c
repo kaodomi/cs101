c1b6b4c43e712c8a66c80d41244cda5174249ee5
kaodomi <151603952+kaodomi@users.noreply.github.com>
2023-12-20 21:21:05 +0800

    Create main2.c

---
+++ hw5/main2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    double mypi=4.0;
+    long calcpi=1;
+    int i;
+    float sign=1.0;
+    
+    for (i=1; i<=1000000 && (calcpi>3141594 || calcpi<3141590) ; i++)
+    {
+        mypi = mypi-sign*4.0/(2*i+1);
+        sign = sign*(-1);
+        calcpi = (long)(mypi*1000000);
+    }
+    
+    printf("times=%d \tvalue=%7.5f \n",i,mypi);
+    
+    return 0;
+}
