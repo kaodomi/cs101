ce0f4cf75d62aab2fc48750cb5b0e6bc55ca0bcd
kaodomi <151603952+kaodomi@users.noreply.github.com>
2023-12-20 23:13:00 +0800

    Update main1.c

--- hw5/main1.c
+++ hw5/main1.c
@@ -1,33 +1,19 @@
 #include <stdio.h>
-int main()
-{
-    int n=7;
-    int rows;
-    int pos;
-    int num;
-    int x;
-    
-    for (rows=1; rows<=n; rows++)
-    {
-        x = n+1-rows;
-        num = rows;
-        
-        for (pos=1 ; pos <= n*2 ; pos++)
-        {
-            if (pos == x && num > 0)
-            {
-                printf("%d", rows);
-                x = x+2;
-                num = num-1;
-            }
-            else
-            {
+int main() {
+    int n = 7;
+    int spc = n;
+    for(int i=1 ; i<=n ; i++){
+        for(int tot=0; tot<=n; tot++){
+            if(tot<spc){
                 printf(" ");
             }
+            else {
+                printf("%d ", i);
+            }
         }
-    
         printf("\n");
+        spc--;
     }
-
+    
     return 0;
 }
